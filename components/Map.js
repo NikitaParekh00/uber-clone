157babe10e8e6d7f09c8c7dea0d9e49da1d2bc43
nikita <nikita.parekh@unboxrobotics.com>
2024-01-02 01:06:00 +0530

    building the map screen component

---
+++ components/Map.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps"; // remove PROVIDER_GOOGLE import if not using Google Maps
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+const Map = () => {
+  return (
+    <MapView
+      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+      style={styles.map}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}
+    ></MapView>
+  );
+};
+
+export default Map;
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,15 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import tw from "twrnc";
+import Map from "../components/Map";
 
 export const MapScreen = () => {
-  return <Text>MapScreen</Text>;
+  return (
+    <View>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+      <View style={tw`h-1/2`}></View>
+    </View>
+  );
 };
